6233383a0b5a168a7d85c635f81e4cc1c4fb5ca7
Benjamin Ibarra <34190205+BeniBarra@users.noreply.github.com>
2022-11-30 16:20:56 -0500

    backend cache generation moved to back end, to loop call of querying function

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,57 +1,40 @@
 import React from 'react';
 import './App.css';
 import PokemonHeader from './components/PokemonHeader'
-import PokemonCard from './components/PokemonCards';
 import SearchBar from './components/SearchBar';
 import generateCache from './components/GenerateCache';
+import LoginButton from './components/LoginButton';
+import LogoutButton from './components/LogoutButton';
+import CardContainer from './components/CardContainer';
 import getPokemonObjs from './components/GetPokemonObjs';
 import getPokemonNames from './components/GetPokemonNames';
-// const generateCache =  require('./components/GenerateCache');
-// const getPokemonObjs = require('./components/GetPokemonObjs');
-// const getPokemonNames = require('./components/GetPokemonNames');
 
 class App extends React.Component {
-
   constructor(props) {
     super(props); 
     this.state = {
       pokemonNameArr : [],
       pokemonObjArr: [],
-      cacheGenerated: false,
     }
-}
-
-  componentDidMount = async () => {
-    generateCache();
-    this.setState({
-      cacheGenerated: true,
-    })
   }
 
-  getPokemonCards = async () => {
-    if(this.state.cacheGenerated) {
-      let pokemonObjsData = await getPokemonObjs();
-      let pokemonNamesData = await getPokemonNames();
-
-      this.setState({
-        pokemonObjArr : pokemonObjsData,
-        pokeNameArr : pokemonNamesData,
-      })
-    } 
+  componentDidMount = async () =>{
+    console.log('pre: ' + this.state.pokemonObjArr);
+    this.setState({
+      pokemonObjArr : await getPokemonObjs(),
+      pokemonNameArr : await getPokemonNames()
+    })
+    console.log('post: ' + this.state.pokemonObjArr);
   }
 
   render() {
-
-    {this.getPokemonCards()}
-
     return (
       <>
         <PokemonHeader/>
         <SearchBar pokemonNames={this.state.pokemonNameArr}/>
-          {
-            this.state.pokemonObjArr ? this.state.pokemonObjArr.map(pokemon => 
-            <PokemonCard name={pokemon.name} id={pokemon.id} description={pokemon.description} moves={pokemon.moves} sprites={pokemon.sprites} stats={pokemon.stats} types={pokemon.types}/> ) : <></>
-          }
+        <LoginButton></LoginButton>
+        <LogoutButton></LogoutButton>
+        <CardContainer pokemonArr={this.state.pokemonObjArr}></CardContainer>
         <button id='test-button' onClick={this.getPokemon151}>Submit</button>
       </>
     );
--- pokemon-app/src/components/GenerateCache.js
+++ pokemon-app/src/components/GenerateCache.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 //require('dotenv').config();
 
 async function generateCache() {
-    await axios.get(process.env.REACT_APP_BACK_END);
+    return await axios.get(process.env.REACT_APP_BACK_END);
 };
 
 export default generateCache;
